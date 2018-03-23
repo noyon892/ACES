0d13a858773372908338805b2b4331b2e9bd6069
Hasanuzzaman Noyon <noyon892@gmail.com>
2018-03-23 23:02:13 +0600

    db

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+    define('HOST','localhost');
+    define('USERNAME','root');
+    define('PASSWORD','');
+    define('DATABASE_NAME','royal_motors_db');
+    $db = mysqli_connect(HOST,USERNAME,PASSWORD,DATABASE_NAME) or die(mysql_error());
+?>
