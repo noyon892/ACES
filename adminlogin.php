2512c7b323c79dccd8f73099005e4f7a35712466
Hasanuzzaman Noyon <noyon892@gmail.com>
2018-03-23 22:46:50 +0600

    Admin login

---
+++ adminlogin.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   
+    <title>AIUB Community of Engineering Students-ACES</title>
+    <!-- Icon -->
+    <link rel="icon" href="images/logo2.png">
+    <!-- Meta Tag -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Style CSS Linking -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css"> -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- JS linking -->
+    <script src="/js/jquery-3.2.1.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/myjs.js"></script>
+  </head>
+  <body>
+
+  	<div class="container-fluid Contact-info">
+  	  <div class="container">
+  	    <div class="row">
+  	      <div class="col-xs-12 col-sm-10 col-md-10 visible">
+  	        <a class="navbar-brand navbar-logo my-nav-logo" href="index.html">
+  	           <div class="row">
+  	           	<div class="col-md-1">
+  	           		<img class="img-responsive" alt="Brand" src="images/logo2.png">
+  	           	</div>
+  	           	<div class="col-md-9 name">
+  	           		<h2 class="text-left">AIUB Community of Engineering Students-ACES</h2>
+  	           	</div>
+  	           </div>
+  	        </a>
+  	      </div>
+  	    </div>
+  	  </div>
+  	</div>
+
+    <nav class="navbar navbar-inverse my-navbar">
+      <div class="container-fluid">
+      	<div class="container">
+      		<!-- Brand and toggle get grouped for better mobile display -->
+      		<div class="navbar-header">
+      		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      		    <span class="sr-only">Toggle navigation</span>
+      		    <span class="icon-bar"></span>
+      		    <span class="icon-bar"></span>
+      		    <span class="icon-bar"></span>
+      		  </button>
+      		  <a class="navbar-brand navbar-logo vanish" href="index.html">
+      		     <img class="img-responsive" alt="Brand" src="images/logo2.png">
+      		  </a>
+      		  
+      		</div>
+
+      		<!-- Collect the nav links, forms, and other content for toggling -->
+      		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      		  <ul class="nav navbar-nav my-nav">
+      		    <li><a href="index.html">Home</a></li>
+      		        <li><a href="#about">About Us</a></li>
+      		        <li><a href="#our-projects">Our Works</a></li>
+      		        <li><a href="#our-team">Our Team</a></li>
+      		        <li><a href="#contact">Contact Us</a></li>
+      		  </ul>
+      		  <ul class="nav navbar-nav navbar-right">
+      		      <li><a href="/login">Login</a></li>
+      		  </ul>
+      		</div><!-- /.navbar-collapse -->
+      			
+      	</div>
+        </div><!-- /.container-fluid -->
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+              <img class="img-responsive" src="images/image3.jpg">
+            </div>
+            <div class="col-md-5 login-panel">
+              <div class="row">
+                <div class="full-panel">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading font">
+                            <span class="glyphicon glyphicon-lock"></span>Admin Login</div>
+                        <div class="panel-body">
+                            <form class="form-horizontal" role="form" method="post">
+                              <div class="form-group">
+                                  <label for="inputEmail3" class="col-sm-3 control-label font">
+                                      Username</label>
+                                  <div class="col-sm-9">
+                                      <input type="text" class="form-control font" id="inputEmail3" placeholder="Username" name="username">
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label for="inputPassword3" class="col-sm-3 control-label font">
+                                      Password</label>
+                                  <div class="col-sm-9">
+                                      <input type="password" class="form-control font" id="inputPassword3" placeholder="Password" name="password">
+                                  </div>
+                              </div>
+                              <div class="form-group last">
+                                  <div class="col-sm-offset-3 col-sm-9">
+                                      <button type="submit" class="btn btn-success btn-sm font glyphicon glyphicon-log-in"> Login</button>
+                                         <button type="reset" class="btn btn-default btn-sm font">
+                                        Reset</button>
+                                  </div>
+                              </div>
+                            </form>
+                          <button type="submit" class=" pull-right btn-link font" disabled><a href="www.google.com">Forget password</a></button><br>
+                        </div>
+                        <div class="panel-footer font">
+                        
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="my-footer-full no-pad">
+      <div class="container-fluid no-pad">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+          <ul class="nav navbar-nav navbar-right my-footer text-center animated wow fadeIn">
+            <li><a href="#">Research</a></li>
+            <li><a href="#about">Magazine</a></li>
+            <li><a href="#our-projects">Alumni</a></li>
+            <li><a href="#our-team">Gallery</a></li>
+          </ul>
+      </div><!-- /.container-fluid -->
+      <hr>
+      <div class="container-fluid">
+            <div class="row">
+                    <!-- First Col -->
+                    <div class="col-sm-4 about-us">
+                      <div class="text-center">
+                        <h2 class=""><strong>About us</strong></h2>  
+                        <p class="">
+                          <strong>
+                           ACES is a non-profitable youth developing foundation focuses on motivating young students to involve in ICT sector. It not only motivates but also makes a platform to engage the students in different ICT related activities.
+                          </strong>
+                        </p>
+                      </div>
+                    </div>
+                    <!-- Middle Col -->
+                    <div class="col-sm-4 col-xs-12 contact" id="contact">
+                      <div class="logo">
+                        <img class="img-responsive" src="images/logo2.png"></img>
+                      </div>
+                      <div class="contact-us">
+                        <h3 class="text-center"><strong>AIUB Community of Engineering Students</strong></h3>
+                        <div class="text-center"><p class="phone"><strong><i class="fa fa-phone" aria-hidden="true"></i>(+88)01**********</strong></p> </div>
+                        <div class="text-center">
+                          <p class="email"><strong><i class="fa fa-envelope-o" aria-hidden="true"></i>aces@yourmail.com</strong>
+                        </div>
+                        <div class="social text-center">
+                          <!-- FaceBook -->
+                          <a href="https://www.facebook.com/csdfoundationbd/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                          <!-- Youtube -->
+                          <a href="https://www.youtube.com/channel/UCmzeyt9l8OtysGFRL5EEQwQ" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                          <!-- Twitter -->
+                          <a href="https://twitter.com/csdfbd" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                          <!-- Google Plus -->
+                          <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                          <!-- Linkedin -->
+                          <a href="https://www.linkedin.com/company-beta/13352994/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- Last Col -->
+                    <div class="col-sm-4 col-xs-12 feedback">
+                        <h2 class="text-center"><strong>Give Us Feedback</strong></h2>
+                        <form class="feedback-form" action="">
+                          <div class="row">
+                              <div class="col-sm-4 col-sm-offset-2 no-pad"><input type="text" class="form-control" name="name" placeholder="Name" required></div>
+                              <div class="col-sm-4 no-pad"><input type="email" class="form-control" name="email" placeholder="Email" required></div>
+                              <div class="col-sm-8 col-sm-offset-2 no-pad"><textarea class="form-control" rows="3" name="comments" placeholder="Your Feedback Here" required></textarea></div>
+                              <div class="col-sm-8 col-sm-offset-2 no-pad"><button type="submit" class="btn btn-primary btn-group-justified my-button">Submit</button></div>
+                            </div>
+                        </form>
+                    </div>
+                  </div>
+      </div>
+    </footer>
+    <div class="copy-right text-center container-fluid">Copyright &copy AIUB Community of Engineering Students-ACES 2018 | All rights reserved.</div>
+
+       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <!-- <script src="js/wow.min.js"></script> -->
+    <script src="js/myjs.js"></script>
+    <!-- Js For Thumbnail Caoursel -->
+    <script type="text/javascript">
+      $(document).ready(function () {
+          $('#myCarousel').carousel({
+              interval: 10000
+          })
+          $('.fdi-Carousel .item').each(function () {
+              var next = $(this).next();
+              if (!next.length) {
+                  next = $(this).siblings(':first');
+              }
+              next.children(':first-child').clone().appendTo($(this));
+
+              if (next.next().length > 0) {
+                  next.next().children(':first-child').clone().appendTo($(this));
+              }
+              else {
+                  $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+              }
+          });
+      });
+    </script>
+  </body>
+</html>
